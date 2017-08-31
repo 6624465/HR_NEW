69f7b83d29cd892edcddc3e166e264820b7a0eaf
Meena Konakondla <meena.konakondla@gmail.com>
2017-08-31 14:01:19 +0530

    changes

--- HR.Core/Models/EmployeeLeaveList.cs
+++ HR.Core/Models/EmployeeLeaveList.cs
@@ -13,9 +13,7 @@ namespace HR.Core.Models
         public int Id { get; set; }
         public int EmployeeId { get; set; }
         public virtual Employee Employee { get; set; }
-        public int LookupId { get; set; }
-        public virtual LookUp Lookup { get; set; }
-
+        public int LeaveTypeId { get; set; }
         public int BranchId { get; set; }
         public virtual Branch Branch { get; set; }
         public DateTime FromDate { get; set; }
