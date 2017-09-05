1465bd1249db1ceca58df8a60a85f1a06a7de80a
Meena Konakondla <meena.konakondla@gmail.com>
2017-09-05 15:01:59 +0530

    changes

---
+++ HR/Areas/Employees/Controllers/EmployeeProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR.Areas.Employees.Controllers
+{
+    public class EmployeeProfileController : Controller
+    {
+        // GET: Employee/EmployeeProfile
+        public ActionResult EmployeeProfile()
+        {
+            return View("~/Areas/Employees/Views/EmployeeProfile/EmployeeProfile.cshtml");
+        }
+    }
+}
