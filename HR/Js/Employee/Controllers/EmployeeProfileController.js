e62c3a351bd335faabcb357f88f2b07a31195dd1
Meena Konakondla <meena.konakondla@gmail.com>
2017-09-05 15:05:23 +0530

    changes

---
+++ HR/Js/Employee/Controllers/EmployeeProfileController.js
@@ -0,0 +1,5 @@
+﻿angular.module('ngHR').controller('EmployeeProfileController', ['$scope', '$http', 'growl', '$filter', 'UtilityFunc', 
+    'growlService', function ($scope, $http, growl, $filter, UtilityFunc, growlService) {
+
+
+}])
