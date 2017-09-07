c077ca8c2c307e4da682a92a98781838c553cbfa
Meena Konakondla <meena.konakondla@gmail.com>
2017-09-07 18:35:23 +0530

    changes

--- HR/Js/Account/Controllers/LoginController.js
+++ HR/Js/Account/Controllers/LoginController.js
@@ -6,7 +6,6 @@ app.config(function (growlProvider) {
     growlProvider.globalTimeToLive({ success: 4000, error: 2000, warning: 3000, info: 4000 });
 })
 app.controller('loginController', ['$scope', '$http', 'LoginService', 'growl', 'growlService', '$timeout', function ($scope, $http, LoginService, growl, growlService,$timeout) {
-    debugger;
     $scope.init = function () {
         $scope.IsEnable = true;
     }
@@ -17,11 +16,10 @@ app.controller('loginController', ['$scope', '$http', 'LoginService', 'growl', '
     }
         var user = { "UserName": $scope.UserName, "Password": $scope.Password };
         LoginService.LogIn(user).then(function (response) {
-            debugger;
             if (response && response.data && response.data.success == true) {
                 $scope.showLoading = false;
                 $scope.IsEnable = true;
-                sessionStorage.setItem('User', JSON.stringify(response.data.SessionObject));
+                sessionStorage.setItem('authenticatedUser', JSON.stringify(response.data.SessionObject));
                 location.href = "Home/index/";
             }
         })
