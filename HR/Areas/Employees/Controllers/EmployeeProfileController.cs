520d623b1eabb60bc7cd423da4be5b52daea4b00
chaitanya <chaitanya@sulabs.tech>
2017-09-08 18:45:19 +0530

    Changes done for apply the getmethod

--- HR/Areas/Employees/Controllers/EmployeeProfileController.cs
+++ HR/Areas/Employees/Controllers/EmployeeProfileController.cs
@@ -12,58 +12,13 @@ namespace HR.Areas.Employees.Controllers
     public class EmployeeProfileController : BaseController
     {
 
-        public JsonResult SaveEmlployee(EmployeeHeader employeeHeader)
+        public JsonResult SaveEmlployee(EmployeePersonalInfo employeePersonalInfo)
         {
             JsonResult result = new JsonResult();
-            if (employeeHeader != null)
+            if (employeePersonalInfo != null)
             {
                 try
                 {
-                    EmployeeHeader _employeeHeader = new EmployeeHeader();
-                    if (employeeHeader.Id > 0)
-                    {
-                        _employeeHeader.ModifiedBy = USER_OBJECT.UserName;
-                        _employeeHeader.ModifiedOn = DateTimeConverter.SingaporeDateTimeConversion(DateTime.Now);
-                    }
-                    else
-                    {
-                        _employeeHeader.CreatedBy = USER_OBJECT.UserName;
-                        _employeeHeader.CreatedOn = DateTimeConverter.SingaporeDateTimeConversion(DateTime.Now);
-                    }
-                    _employeeHeader = PrepareEmployeeHeader(employeeHeader);
-
-                    EmployeeProfileService.SaveEmployeeHeader(employeeHeader);
-                    result = Json(new { sucess = true, message = "Sent successfully" }, JsonRequestBehavior.AllowGet);
-                }
-                catch (Exception ex)
-                {
-                    if (ex.InnerException != null && !string.IsNullOrEmpty(ex.InnerException.Message))
-                        return Json(new { success = false, message = ex.InnerException.Message }, JsonRequestBehavior.DenyGet);
-                }
-            }
-            return result;
-        }
-
-        private EmployeeHeader PrepareEmployeeHeader(EmployeeHeader employeeHeader)
-        {
-            EmployeeHeader _employeeHeader = new EmployeeHeader();
-            EmployeePersonalInfo employeePersonalInfo = employeeHeader.EmployeePersonalInfo;
-            _employeeHeader.BranchId = employeeHeader.BranchId;
-            _employeeHeader.EmployeeId = employeeHeader.EmployeeId > 0 ? employeeHeader.EmployeeId : 0;
-            _employeeHeader.FirstName = string.IsNullOrWhiteSpace(employeeHeader.FirstName) ? employeeHeader.FirstName : string.Empty;
-            _employeeHeader.MiddleName = string.IsNullOrWhiteSpace(employeeHeader.MiddleName) ? employeeHeader.MiddleName : string.Empty;
-            _employeeHeader.LastName = string.IsNullOrWhiteSpace(employeeHeader.LastName) ? employeeHeader.LastName : string.Empty;
-            _employeeHeader.Nationality = string.IsNullOrWhiteSpace(employeeHeader.Nationality) ? employeeHeader.Nationality : string.Empty;
-            _employeeHeader.IDNumber = string.IsNullOrWhiteSpace(employeeHeader.IDNumber) ? employeeHeader.IDNumber : string.Empty;
-            _employeeHeader.IDType = employeeHeader.IDType;
-            _employeeHeader.EmployeePersonalInfo = PrepareEmployeePersonalInfo(employeePersonalInfo);
-            _employeeHeader.Address = PrepareEmployeeAddress(employeeHeader.Address);
-            _employeeHeader.EmployeeWorkDetail = PrepareEmployeeWorkDetail(employeeHeader.EmployeeWorkDetail);
-            return _employeeHeader;
-        }
-
-        private EmployeePersonalInfo PrepareEmployeePersonalInfo(EmployeePersonalInfo employeePersonalInfo)
-        {
                     EmployeePersonalInfo _employeePersonalInfo = new EmployeePersonalInfo();
                     if (employeePersonalInfo.Id > 0)
                     {
@@ -76,68 +31,31 @@ namespace HR.Areas.Employees.Controllers
                         _employeePersonalInfo.CreatedBy = USER_OBJECT.UserName;
                         _employeePersonalInfo.CreatedOn = DateTimeConverter.SingaporeDateTimeConversion(DateTime.Now);
                     }
-            _employeePersonalInfo.BranchId = employeePersonalInfo.BranchId;
-            _employeePersonalInfo.DOB = DateTimeConverter.SingaporeDateTimeConversion(employeePersonalInfo.DOB),
+                    _employeePersonalInfo.DOB = employeePersonalInfo.DOB;
                     _employeePersonalInfo.Gender = employeePersonalInfo.Gender;
-            _employeePersonalInfo.FatherName = employeePersonalInfo.FatherName;
                     _employeePersonalInfo.BirthCountry = employeePersonalInfo.BirthCountry;
+                    _employeePersonalInfo.CitizenshipCountry = employeePersonalInfo.CitizenshipCountry;
+                    _employeePersonalInfo.FatherName = employeePersonalInfo.FatherName;
                     _employeePersonalInfo.MaritalStatus = employeePersonalInfo.MaritalStatus;
                     _employeePersonalInfo.SpouseName = employeePersonalInfo.SpouseName;
-            _employeePersonalInfo.MarriageDate = DateTimeConverter.SingaporeDateTimeConversion(employeePersonalInfo.MarriageDate.Value);
+                    _employeePersonalInfo.MarriageDate = employeePersonalInfo.MarriageDate;
                     _employeePersonalInfo.ResidentialStatus = employeePersonalInfo.ResidentialStatus;
 
-            return _employeePersonalInfo;
-        }
-        private Address PrepareEmployeeAddress(Address address)
-        {
-            Address _address = new Address();
-            if (address.AddressId > 0)
-            {
-                _address.ModifiedBy = USER_OBJECT.UserName;
-                _address.ModifiedOn = DateTimeConverter.SingaporeDateTimeConversion(DateTime.Now);
-            }
-            else
-            {
-                _address.CreatedBy = USER_OBJECT.UserName;
-                _address.CreatedOn = DateTimeConverter.SingaporeDateTimeConversion(DateTime.Now);
-            }
-            _address.Address1 = address.Address1;
-            _address.AddressType = address.AddressType;
-            _address.CityName = address.CityName;
-            _address.StateName = address.StateName;
-            _address.ZipCode = address.ZipCode;
-            _address.MobileNo = address.MobileNo;
-            _address.CountryCode = address.CountryCode;
-            return _address;
+                    EmployeeProfileService.SaveEmployeeProfile(employeePersonalInfo);
+                    result = Json(new { sucess = true, message = "Sent successfully" }, JsonRequestBehavior.AllowGet);
                 }
-        private EmployeeWorkDetail PrepareEmployeeWorkDetail(EmployeeWorkDetail employeeWorkDetail)
-        {
-            EmployeeWorkDetail _employeeWorkDetail = new EmployeeWorkDetail();
-            if (employeeWorkDetail.Id > 0)
+                catch (Exception ex)
                 {
-                _employeeWorkDetail.ModifiedBy = USER_OBJECT.UserName;
-                _employeeWorkDetail.ModifiedOn = DateTimeConverter.SingaporeDateTimeConversion(DateTime.Now);
+                    if (ex.InnerException != null && !string.IsNullOrEmpty(ex.InnerException.Message))
+                        return Json(new { success = false, message = ex.InnerException.Message }, JsonRequestBehavior.DenyGet);
                 }
-            else
-            {
-                _employeeWorkDetail.CreatedBy = USER_OBJECT.UserName;
-                _employeeWorkDetail.CreatedOn = DateTimeConverter.SingaporeDateTimeConversion(DateTime.Now);
             }
-            _employeeWorkDetail.BranchId = employeeWorkDetail.BranchId;
-            _employeeWorkDetail.JoiningDate = employeeWorkDetail.JoiningDate;
-            _employeeWorkDetail.ConfirmationDate = employeeWorkDetail.ConfirmationDate;
-            _employeeWorkDetail.ProbationPeriod = employeeWorkDetail.ProbationPeriod;
-            _employeeWorkDetail.NoticePeriod = employeeWorkDetail.NoticePeriod;
-            _employeeWorkDetail.Designation = employeeWorkDetail.Designation;
-            _employeeWorkDetail.Department = employeeWorkDetail.Department;
-            return _employeeWorkDetail;
+            return result;
         }
-
-        #region ActionResult
+        // GET: Employee/EmployeeProfile
         public ActionResult EmployeeProfile()
         {
             return View();
         }
-        #endregion
     }
 }
