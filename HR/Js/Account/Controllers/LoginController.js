26b7d6e5bcfefaba5a223629c72a63e169e0da6e
Meena Konakondla <meena.konakondla@gmail.com>
2017-09-08 11:19:39 +0530

    focus for login page and issue in login page

--- HR/Js/Account/Controllers/LoginController.js
+++ HR/Js/Account/Controllers/LoginController.js
@@ -5,27 +5,51 @@ app.config(function (growlProvider) {
     growlProvider.onlyUniqueMessages(false);
     growlProvider.globalTimeToLive({ success: 4000, error: 2000, warning: 3000, info: 4000 });
 })
-app.controller('loginController', ['$scope', '$http', 'LoginService', 'growl', 'growlService', '$timeout', function ($scope, $http, LoginService, growl, growlService,$timeout) {
+
+app.directive('focusOn', function () {
+    return function (scope, elem, attr) {
+        scope.$on('focusOn', function (e, name) {
+            if (name === attr.focusOn) {
+                elem[0].focus();
+            }
+        });
+    };
+});
+
+app.factory('focus', function ($rootScope, $timeout) {
+    return function (name) {
+        $timeout(function () {
+            $rootScope.$broadcast('focusOn', name);
+        });
+    }
+});
+app.controller('loginController', ['$scope', '$http', 'LoginService', 'growl', 'growlService', '$timeout', 'focus',
+    function ($scope, $http, LoginService, growl, growlService, $timeout, focus) {
+        focus('UserName');
     $scope.init = function () {
-        $scope.IsEnable = true;
+        $scope.IsEnable = false;
+        $scope.User = {}
     }
     $scope.login = function () {
-        if ($scope.UserName != null && $scope.Password != null){
-        $scope.showLoading = true;
-        $scope.IsEnable = false;
+        if ($scope.User.UserName != null && $scope.User.Password != null) {
+            $scope.showLoading = true;
+            $scope.IsEnable = true;
+            LoginService.LogIn($scope.User).then(function (response) {
+                if (response && response.data && response.data.success == true) {
+                    $scope.showLoading = false;
+                    //$scope.IsEnable = true;
+                    sessionStorage.setItem('authenticatedUser', JSON.stringify(response.data.SessionObject));
+                    location.href = "Home/index/";
+                }
+                else {
+                    $scope.showLoading = false;
+                    $scope.IsEnable = false;
+                    growlService.growl("Sorry, your login information is incorrect.", 'danger');
+                    $scope.User = {};
+                    focus('UserName');
+                }
+            })
+        }
     }
-        var user = { "UserName": $scope.UserName, "Password": $scope.Password };
-        LoginService.LogIn(user).then(function (response) {
-            if (response && response.data && response.data.success == true) {
-                $scope.showLoading = false;
-                $scope.IsEnable = true;
-                sessionStorage.setItem('authenticatedUser', JSON.stringify(response.data.SessionObject));
-                location.href = "Home/index/";
-            }
-            else {
-                $scope.showLoading = false;
-                $scope.IsEnable = false;
-            }
-        })
-    }
+    $scope.init();
 }]);
