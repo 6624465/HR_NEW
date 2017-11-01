2748fc150645a79a6a234d883508477e7d4c91f6
Shruthi.T <tshruthi77@gmail.com>
2017-11-01 18:56:11 +0530

    changes in employee type

--- HR/Js/Master/MasterController/maincontroller.js
+++ HR/Js/Master/MasterController/maincontroller.js
@@ -6,95 +6,48 @@
     $scope.statustype = false;
     $scope.marriageStatus = false;
 
-    //$scope.onClickDesignation = function () {
-    //    $scope.designation = true;
-    //    $scope.emptype = false;
-    //    $scope.depttype = false;
-    //    $scope.statustype = false;
-    //    $scope.marriageStatus = false;
-    //};
 
-    //$scope.onClickType = function () {
-    //    $scope.emptype = true;
-    //    $scope.designation = false;
-    //    $scope.depttype = false;
-    //    $scope.statustype = false;
-    //    $scope.marriageStatus = false;
-    //};
-    //$scope.onClickDeptType = function () {
-    //    $scope.depttype = true;
-    //    $scope.designation = false;
-    //    $scope.statustype = false;
-    //    $scope.emptype = false;
-    //    $scope.marriageStatus = false;
-    //};
-    //$scope.onClickStatusType = function () {
-    //    $scope.statustype = true;
-    //    $scope.designation = false;
-    //    $scope.emptype = false;
-    //    $scope.depttype = false;
-    //    $scope.marriageStatus = false;
-    //};
-
-    //$scope.onClickMarriageStatusType = function () {
-    //    $scope.statustype = false;
-    //    $scope.designation = false;
-    //    $scope.emptype = false;
-    //    $scope.depttype = false;
-    //    $scope.marriageStatus = true;
-
-    //}
-  
-    //$scope.lookup = [{ LookUpCategory: "Employee Department" },
-    //                 { LookUpCategory: "EmployeeType" },
-    //                 { LookUpCategory: "EmployeeStatus" },
-    //                 { LookUpCategory: "EmployeeDesignation" },
-    //                 { LookUpCategory: "MaritalStatus" }];
+    $scope.LookUpCategory = 'Employee Designation';
     $scope.selectChanged = function (search) {
-        //if (search == undefined || search == null || search == "") {
-        //    $scope.designation = false;
-        //    $scope.emptype = false;
-        //    $scope.depttype = false;
-        //    $scope.statustype = false;
-        //    $scope.marriageStatus = false;
-        //} else {
-            debugger
-            if (search.LookUpCategory == 'EmployeeDesignation') {
-                $scope.designation = true;
-                $scope.emptype = false;
-                $scope.depttype = false;
-                $scope.statustype = false;
-                $scope.marriageStatus = false;
-            } else if (search.LookUpCategory == 'EmployeeDepartment') {
-                $scope.depttype = true;
-                $scope.designation = false;
-                $scope.statustype = false;
-                $scope.emptype = false;
-                $scope.marriageStatus = false;
-            }
-            else if (search.LookUpCategory == 'EmployeeType') {
-                $scope.emptype = true;
-                $scope.designation = false;
-                $scope.depttype = false;
-                $scope.statustype = false;
-                $scope.marriageStatus = false;
-            } else if (search.LookUpCategory == 'EmployeeStatus') {
-                $scope.statustype = true;
-                $scope.designation = false;
-                $scope.emptype = false;
-                $scope.depttype = false;
-                $scope.marriageStatus = false;
-            } else {
-                $scope.statustype = false;
-                $scope.designation = false;
-                $scope.emptype = false;
-                $scope.depttype = false;
-                $scope.marriageStatus = true;
-            }
-        //}
-            };
+        if (search.LookUpCategory == 'Employee Designation') {
+            $scope.LookUpCategory = search.LookUpCategory;
+            $scope.designation = true;
+            $scope.emptype = false;
+            $scope.depttype = false;
+            $scope.statustype = false;
+            $scope.marriageStatus = false;
+        } else if (search.LookUpCategory == 'Employee Department') {
+            $scope.LookUpCategory = search.LookUpCategory;
+            $scope.depttype = true;
+            $scope.designation = false;
+            $scope.statustype = false;
+            $scope.emptype = false;
+            $scope.marriageStatus = false;
+        }
+        else if (search.LookUpCategory == 'Employee Type') {
 
-   
+            $scope.LookUpCategory = search.LookUpCategory;
+            $scope.emptype = true;
+            $scope.designation = false;
+            $scope.depttype = false;
+            $scope.statustype = false;
+            $scope.marriageStatus = false;
+        } else if (search.LookUpCategory == 'Employee Status') {
+            $scope.LookUpCategory = search.LookUpCategory;
+            $scope.statustype = true;
+            $scope.designation = false;
+            $scope.emptype = false;
+            $scope.depttype = false;
+            $scope.marriageStatus = false;
+        } else {
+            $scope.LookUpCategory = search.LookUpCategory;
+            $scope.statustype = false;
+            $scope.designation = false;
+            $scope.emptype = false;
+            $scope.depttype = false;
+            $scope.marriageStatus = true;
+        }
 
+    };
     
 }]);
