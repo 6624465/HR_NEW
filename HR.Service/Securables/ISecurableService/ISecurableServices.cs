02cf79b41d93858a547ec0ea46f426334a8dfe19
Meena Konakondla <meena.konakondla@gmail.com>
2017-11-07 12:35:27 +0530

    added missing files

---
+++ HR.Service/Securables/ISecurableService/ISecurableServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.Core.Models;
+using System.Linq.Expressions;
+
+namespace HR.Service.Securables.ISecurableService
+{
+    public interface ISecurableServices
+    {
+        IQueryable<T> GetSecurable<T>(Expression<Func<T, bool>> predicate = null) where T : Securable;
+    }
+}
---
+++ HR.Service/Securables/SecurableService/SecurableServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.Service.Securables.ISecurableService;
+using Ninject;
+using HR.Data.BaseRepositories;
+using HR.Core.Models;
+using System.Linq.Expressions;
+
+namespace HR.Service.Securables.SecurableService
+{
+    public class SecurableServices : ISecurableServices
+    {
+
+        [Inject]
+        public IRepository<Securable> SecurablesRepository { get; set; }
+
+        public IQueryable<T> GetSecurable<T>(Expression<Func<T, bool>> predicate = null) where T : Securable
+        {
+            //throw new NotImplementedException();
+            var query = SecurablesRepository.FindAll().OfType<T>();
+            if (predicate != null)
+                query = query.Where(predicate);
+            return query;
+        }
+    }
+}
